06fa5ebffc998c5fd9dab92376812b8814e1cdf5
Rock0421 <130579211+Rock0421@users.noreply.github.com>
2023-04-16 21:33:21 +0800

    Update run_1.3b.sh

--- run_1.3b.sh
+++ run_1.3b.sh
@@ -15,6 +15,6 @@ if [ "$ZERO_STAGE" == "" ]; then
 fi
 mkdir -p $OUTPUT
 
-deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-1.3b \
-   --gradient_accumulation_steps 2 --lora_dim 128 --zero_stage $ZERO_STAGE --only_optimize_lora --per_device_train_batch_size 1 --per_device_eval_batch_size 1 \
+deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-350 \
+   --gradient_accumulation_steps 2 --lora_dim 128 --zero_stage $ZERO_STAGE \
    --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
